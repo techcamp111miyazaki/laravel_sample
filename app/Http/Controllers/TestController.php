4dea78e47e0f4506f1fc0ceb4df5563ad28ac376
techcamp111miyazaki <kirohi19980715@gmail.com>
2022-01-29 16:41:57 +0900

    npmのインストール

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     //
     public function index(){
         $values = Test::all();
+        $tests = DB::table('tests')->get();
 
-        // dd($values);
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test', [TestController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
