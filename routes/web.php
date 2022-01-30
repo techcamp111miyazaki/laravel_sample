170ac8c9258691ac8a7991ec7947250e6cb781b8
techcamp111miyazaki <kirohi19980715@gmail.com>
2022-01-30 13:58:36 +0900

    詳細ページの作成

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'],function(){
     Route::get('index', [ContactFormController::class, 'index'])->name('contact.index');
     Route::get('create', [ContactFormController::class, 'create'])->name('contact.create');
     Route::post('store', [ContactFormController::class, 'store'])->name('contact.store');
+    Route::get('show/{id}', [ContactFormController::class, 'show'])->name('contact.show');
 });
 
 Auth::routes();
